92dc3b454bfe0d09d640d06048b5ffc78a28e2db
Tuananhdeptrai16 <Tuananhpro1608@gmail.com>
2024-09-13 17:01:27 +0700

    done page not found

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@
     The code was written by programmer Truong Tuan Anh
     Thanks for watching and sharing
 */
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import MyLayOut from "./layouts/MyLayouts/layout";
+import StoreContext from "./db/context";
+import { PageNotFound } from "./pages/404Page/pagenotfound";
 
-const App = () => (
-  <Router>
-    <MyLayOut></MyLayOut>
-  </Router>
-);
+const AppContent = () => {
+  const [page, setPage] = useState(true);
+  const location = useLocation();
+  useEffect(() => {
+    const validPaths = ["/codelab/", "/codelab/home"];
+    if (validPaths.includes(location.pathname)) {
+      setPage(true);
+    } else {
+      setPage(false);
+    }
+  }, [location]);
+
+  return (
+    <StoreContext.Provider value={{ location }}>
+      {page === true ? <MyLayOut /> : <PageNotFound></PageNotFound>}
+    </StoreContext.Provider>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
+
 export default App;
